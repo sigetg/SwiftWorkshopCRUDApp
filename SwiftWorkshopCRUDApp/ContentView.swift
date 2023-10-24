329154a414930da6dc3b55ddd50608c6d51c21e6
George Sigety <89882158+sigetg@users.noreply.github.com>
2023-10-23 20:57:25 -0400

    Initial Commit

---
+++ SwiftWorkshopCRUDApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftWorkshopCRUDApp
+//
+//  Created by George Sigety on 10/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftWorkshopCRUDApp/SwiftWorkshopCRUDAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWorkshopCRUDAppApp.swift
+//  SwiftWorkshopCRUDApp
+//
+//  Created by George Sigety on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWorkshopCRUDAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
