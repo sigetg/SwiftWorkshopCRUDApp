6f55331c05bdfb6ce5841175c358b15949c2d966
George Sigety <89882158+sigetg@users.noreply.github.com>
2023-10-30 20:33:57 -0400

    Simplify and add comments

--- SwiftWorkshopCRUDApp/ToDo.swift
+++ SwiftWorkshopCRUDApp/ToDo.swift
@@ -10,14 +10,14 @@ import SwiftData
 
 @Model // Right click and choose show macro to show some more detailed code added automatically by apple!
 class ToDo: Identifiable {
-    var id: String
+    var id = UUID().uuidString
     var item = ""
     var reminderIsOn = false
     var dueDate = Date.now + (60*60*24)
     var notes = ""
     var isCompleted = false
     
-    init(item: String = "", reminderIsOn: Bool = false, dueDate: Date = Date.now + (60*60*24), notes: String = "", isCompleted: Bool = false) {
+    init(id: String = UUID().uuidString, item: String = "", reminderIsOn: Bool = false, dueDate: Date = Date.now + (60*60*24), notes: String = "", isCompleted: Bool = false) {
         self.id = UUID().uuidString
         self.item = item
         self.reminderIsOn = reminderIsOn
